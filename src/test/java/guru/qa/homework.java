4d0fe025c681edc29f6353f56dd03ecdeccedcb3
Petr <petr.shabunin@ural.rt.ru>
2022-09-26 00:26:23 +0500

    Init test commit from console

---
+++ src/test/java/guru/qa/homework.java
@@ -0,0 +1,11 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class homework {
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2 > 1);
+    }
+}
