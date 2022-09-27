4f30af9659f6d98b40563c3b742515b40fb54491
Petr <petr.shabunin@ural.rt.ru>
2022-09-28 00:29:32 +0500

    some change

--- src/test/java/guru/qa/homework.java
+++ src/test/java/guru/qa/homework.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class homework {
     @Test
     void exampleTest(){
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(3 > 1);
     }
 }
