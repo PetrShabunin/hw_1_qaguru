2478e3f8fdefcccaac174b080a6b199335513776
Petr <petr.shabunin@ural.rt.ru>
2022-09-28 17:35:52 +0500

    added some test

--- src/test/java/guru/qa/HomeworkLession2.java
+++ src/test/java/guru/qa/HomeworkLession2.java
@@ -3,9 +3,9 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class homework {
+public class HomeworkLession2 {
     @Test
     void exampleTest(){
-        Assertions.assertTrue(11 > 2);
+        Assertions.assertTrue(11 > 1);
     }
 }
---
+++ src/test/java/guru/qa/HomeworkLession3.java
@@ -0,0 +1,28 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+
+public class HomeworkLession3 {
+    @BeforeAll
+    static void testFirst(){
+        System.out.println("### all test started"); //вызывается перед всеми тестами 1 раз
+    }
+    @BeforeEach
+    void testEach(){
+        System.out.println("### test started"); // вызывается перед каждым тестом
+    }
+    @Test
+    void simpleTest(){
+        System.out.println("### test ongoing");
+        Assertions.assertTrue(3 > 2);
+    }
+    @AfterEach
+    void testCompleted(){
+        System.out.println("### test completed"); // вызывается после каждого теста
+    }
+    @AfterAll
+    static void TestOut(){
+        System.out.println("### test over"); // вызывается после всех тестов
+    }
+}
