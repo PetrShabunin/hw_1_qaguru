d75ee86397be149d6d312ec914f65da1111e57a5
Petr <petr.shabunin@ural.rt.ru>
2022-09-30 18:56:17 +0500

    123

--- src/test/java/guru/qa/TestClass.java
+++ src/test/java/guru/qa/TestClass.java
@@ -7,15 +7,9 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestClass {
-    @BeforeAll
-    static void setUp(){
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
-    }
-
     @Test
     void someTest() {
-        open("/text-box");
+        open("https://demoqa.com/text-box");
     }
 }
 
